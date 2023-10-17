b5d4889864e06620d5e77a10a11e4fa662aa9bcf
Yang Yi <100780376+YiYang-github@users.noreply.github.com>
2023-10-17 23:10:13 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deepgym",
+    version="0.1.0",
+    author="Jiaxuan You",
+    author_email="jiaxuan@cs.stanford.edu",
+    description="Deepgym: platform for relational database benchmark RDBench",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JiaxuanYou/DeepGym",
+    packages=setuptools.find_packages(),
+    install_requires=[
+      'matplotlib',
+      'networkx',
+      'numpy',
+      'pandas',
+      'scikit_learn',
+      'torch',
+      'torch_geometric',
+      'xgboost'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=tbf',
+)
